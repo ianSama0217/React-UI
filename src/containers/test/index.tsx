bdf82bd18d35fb5e18dc5c6bea7a7366bc2be56e
ianSama0217 <ian20000217@gmail.com>
2024-12-11 11:14:14 +0800

    switch component

--- src/containers/test/index.tsx
+++ src/containers/test/index.tsx
@@ -1,42 +1,44 @@
 import React, { useState } from "react";
 import BasicLayout from "../../components/basicLayout";
-import Button from "../../components/button";
+import Switch from "../../components/switch";
 
 const TestPage: React.FC = () => {
   const [dis, setDis] = useState<boolean>(false);
 
+  function handleChange() {
+    setDis(!dis);
+  }
+
   const components = [
-    <Button
-      title="測試按鈕"
-      style="primary"
-      startIcon={<i className="fa-solid fa-location-dot"></i>}
-      disabled={dis}
-      onClick={() => {
-        setDis(true);
-      }}
+    <Switch title="wifi" isChecked={dis} onChange={handleChange} />,
+    <Switch title="abc" isChecked={true} />,
+    // <Switch title="Test" isChecked={true} />,
+    <Switch
+      title="test"
+      type="square"
+      isChecked={dis}
+      onChange={handleChange}
     />,
-    <Button title="測試按鈕" style="secondary" size="btn-sm" disabled={dis} />,
-    <Button
-      style="success"
-      startIcon={<i className="fa-regular fa-share-from-square"></i>}
-      onClick={() => {
-        setDis(true);
-      }}
-    />,
-    <Button title="測試按鈕" style="outline-warning" size="btn-lg" />,
-    <Button
-      title="測試按鈕"
-      style="danger"
-      endIcon={<i className="fa-solid fa-location-dot"></i>}
-    />,
+    <Switch title="測試" type="square" isChecked={true} />,
+    // <Switch title="測試" type="square" isChecked={true} disabled={true} />,
   ];
 
   const codeContent = `
-    <Button title="測試按鈕" style="primary" />
-    <Button title="測試按鈕" style="secondary" />
-    <Button title="測試按鈕" style="success" />
-    <Button title="測試按鈕" style="warning" />
-    <Button title="測試按鈕" style="danger" />
+    const [dis, setDis] = useState<boolean>(false);
+
+    function handleChange() {
+      setDis(!dis);
+    }
+
+    <Switch title="wifi" isChecked={dis} onChange={handleChange} />
+    <Switch title="abc" isChecked={true} />
+    <Switch
+      title="test"
+      type="square"
+      isChecked={dis}
+      onChange={handleChange}
+    />
+    <Switch title="測試" type="square" isChecked={true} />
 `;
 
   return (
@@ -47,8 +49,8 @@ const TestPage: React.FC = () => {
         components={components}
         code={codeContent}
       />
-      <span style={{ color: "white", fontSize: "1rem" }}>Button</span>
-      <Button title="測試按鈕" />
+      <span style={{ color: "white", fontSize: "1rem" }}>Switch</span>
+      <Switch isChecked={false} />
     </div>
   );
 };
