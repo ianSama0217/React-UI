e6aced69ebeab9481c87190b07b8099e8825de81
ianSama0217 <ian20000217@gmail.com>
2025-02-09 22:06:25 +0800

    slider container

---
+++ src/containers/sliderPage/info.tsx
@@ -0,0 +1,11 @@
+import Slider from "../../components/slider";
+
+export const SliderSize = [
+  <Slider title="slider-sm" size="slider-sm" />,
+  <Slider title="slider-lg" size="slider-lg" />,
+];
+
+export const SliderSizeContent = `    
+    <Slider title="slider-sm" size="slider-sm" />
+    <Slider title="slider-lg" size="slider-lg" />
+      `;
